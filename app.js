f2c2b1f3b2b031394fcfb8b1dca32b011e282b4a
JaronArmiger <jaron.armiger@gmail.com>
2020-09-15 16:22:23 -0500

    configure connection to MongoDB Atlas

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+
+const mongoose = require('mongoose');
+const mongoDB = process.env.MONGODB_URI;
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
