8a8dcdb407088e4414921c386ed4073d8bdce7c8
JaronArmiger <jaron.armiger@gmail.com>
2020-09-15 17:57:25 -0500

    implement population of two sample users in db

--- populatedb.js
+++ populatedb.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const passwordMethods = require('./helpers/password.js');
+
 console.log('populating db :O');
 
 const async = require('async');
@@ -17,4 +19,51 @@ db.on('error', console.error.bind(console, 'MongoDB connection error'));
 const users = [];
 const posts = [];
 const comments = [];
+
+const userCreate = (username, password, cb) => {
+  const saltHash = passwordMethods.genPassword(password);
+  const user = new User({
+    username,
+    salt: saltHash.salt,
+    hash: saltHash.hash,
+  });
+
+  user.save((err) => {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+    console.log('New User: ' + user);
+    users.push(user);
+    cb(null, user);
+    return;
+  });
+};
+
+const postCreate = () => {
+
+};
+
+const createUsers = (cb) => {
+  async.series([
+    function(callback) {
+      userCreate('Kingston', 'Kingston', callback);
+    },
+    function(callback) {
+      userCreate('Girlpool', 'Girlpool', callback);
+    }
+  ], cb);
+};
+
+async.series([
+  createUsers,
+], function(err, results) {
+	 if (err) {
+	 	console.log('FINAL ERR: ' + err);
+	 } else {
+	 	console.log('Results: ' + results)
+	 }
+     mongoose.connection.close();
+   }
+);
 
