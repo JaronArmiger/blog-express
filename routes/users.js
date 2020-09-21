7b8104f96a7d74c558e5d80a34ced808a8623c1e
JaronArmiger <jaron.armiger@gmail.com>
2020-09-21 17:22:25 -0500

    write login route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,24 @@ const User = require('../models/user');
 const utils = require('../lib/utils');
 
 router.post('/login', (req, res, next) => {
-  
+  User.findOne({ username: req.body.username })
+    .then((user) => {
+      if (!user) {
+      	res.status(401).json({ success: false, msg: 'could not find user' });
+      }
+      const isValid = utils.validPassword(req.body.password, user.hash, user.salt);
+      if (isValid) {
+      	const tokenObject = utils.issueJWT(user);
+      	res.status(200).json({ 
+      		success: true, token: tokenObject.token, 
+      		expiresIn: tokenObject.expires });
+      } else {
+      	res.status(401).json({ success: false, msg: 'incorrect password' });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 router.post('/register', (req, res, next) => {
